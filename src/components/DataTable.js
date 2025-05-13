6ed9365b3ebe51e1c357849d22985736656b161f
Medeep Sharma <mithun99@Medeeps-MacBook-Pro.local>
2025-05-13 16:26:14 +0545

    Array implementation completed, working on showing details for values

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -45,203 +45,63 @@ export default function DataTable(props) {
     }
   };
 
-  const requiredDataCenter = [
-    tableData["Singapore"],
-    tableData["India Mumbai"],
-    tableData["India Chennai"],
-    tableData["Emirates"],
-    tableData["US Dallas"],
-  ]
- console.log(requiredDataCenter);
   return (
     <div className="bg-gray-100 p-6">
-      <section className="container mx-auto">
-        <div className="overflow-hidden rounded-xl shadow-md bg-white">
-          <div className="overflow-x-auto">
-            <table className="min-w-full text-sm text-left odd:bg-white even:bg-gray-50 ">
-              <thead className="text-xs font-semibold text-gray-700 bg-gray-200 uppercase border-none shadow-sm">
-                <tr className="border-none">
-                  <th className="px-6 py-4">Data Centers</th>
-                  <th className="px-6 py-4">Capacity</th>
-                  <th className="px-6 py-4">Load</th>
-                  <th className="px-6 py-4">Status</th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200 ">
-                <tr className="hover:bg-gray-100 ">
-                  <td className="px-6 py-4">
-                    <div className="flex items-center">
-                      <div>
-                        <p className="font-medium text-gray-900">Singapore</p>
-                        <p className="text-sm text-gray-500">Main</p>
-                      </div>
+  <section className="container mx-auto">
+    <div className="overflow-hidden rounded-xl shadow-md bg-white">
+      <div className="overflow-x-auto">
+        <table className="min-w-full text-sm text-left odd:bg-white even:bg-gray-50">
+          <thead className="text-xs font-semibold text-gray-700 bg-gray-200 uppercase border-none shadow-sm">
+            <tr className="border-none">
+              <th className="px-6 py-4">Data Centers</th>
+              <th className="px-6 py-4">Capacity</th>
+              <th className="px-6 py-4">Load</th>
+              <th className="px-6 py-4">Status</th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {["Singapore", "India Mumbai", "India Chennai", "Emirates", "US Dallas"].map((location) => (
+              <tr key={location} className="hover:bg-gray-50">
+                <td className="px-6 py-4">
+                  <div className="flex items-center">
+                    <div>
+                      <p className="font-medium text-gray-900">{location.split(" ")[0]}</p>
+                      {location.includes(" ") && (
+                        <p className="text-sm text-gray-500">{location.split(" ").slice(1).join(" ")}</p>
+                      )}
                     </div>
-                  </td>
-                  <td className="px-8 py-4 font-semibold text-gray-700">
-                    {tableData["Singapore"]
-                      ? tableData["Singapore"].capacity
-                      : "N/A"}
-                  </td>
-                  <td className="px-6 py-4">
-                    <span
-                      className={`inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold ${decideColor(
-                        tableData["Singapore"]
-                      )}`}
-                    >
-                      {tableData["Singapore"]
-                        ? tableData["Singapore"].load
-                        : "N/A"}
-                    </span>
-                  </td>
-                  <td className="px-6 py-4">
-                    <span
-                      className={`inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold ${checkStatus(
-                        tableData["Singapore"]
-                      )}`}
-                    >
-                      {checkAvailability(tableData["Singapore"])
-                        ? "Online"
-                        : "Offline"}
-                    </span>
-                  </td>
-                </tr>
-                <tr className="hover:bg-gray-50">
-                  <td className="px-6 py-4">
-                    <div className="flex items-center">
-                      <div>
-                        <p className="font-medium text-gray-900">India</p>
-                        <p className="text-sm text-gray-500">Mumbai</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-8 py-4 font-semibold text-gray-700">
-                    {tableData["India Mumbai"]
-                      ? tableData["India Mumbai"].capacity
-                      : "N/A"}
-                  </td>
-                  <td className="px-6 py-4">
-                    <span
-                      className={`inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold ${decideColor(
-                        tableData["India Mumbai"]
-                      )}`}
-                    >
-                      {tableData["India Mumbai"]
-                        ? tableData["India Mumbai"].load
-                        : "N/A"}
-                    </span>
-                  </td>
-                  <td className="px-6 py-4">
-                    <span className="inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold text-green-800 bg-green-300">
-                      {checkAvailability(tableData["India Mumbai"])
-                        ? "Online"
-                        : "Offline"}
-                    </span>
-                  </td>
-                </tr>
-                <tr className="hover:bg-gray-50">
-                  <td className="px-6 py-4">
-                    <div className="flex items-center">
-                      <div>
-                        <p className="font-medium text-gray-900">India</p>
-                        <p className="text-sm text-gray-500">Chennai</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-8 py-4 font-semibold text-gray-700">
-                    {tableData["India Chennai"]
-                      ? tableData["India Chennai"].capacity
-                      : "N/A"}
-                  </td>
-                  <td className="px-6 py-4">
-                    <span
-                      className={`inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold ${decideColor(
-                        tableData["India Chennai"]
-                      )}`}
-                    >
-                      {tableData["India Chennai"]
-                        ? tableData["India Chennai"].load
-                        : "N/A"}
-                    </span>
-                  </td>
-                  <td className="px-6 py-4">
-                    <span className="inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold text-green-800 bg-green-300">
-                      {checkAvailability(tableData["India Chennai"])
-                        ? "Online"
-                        : "Offline"}
-                    </span>
-                  </td>
-                </tr>
-                <tr className="hover:bg-gray-50">
-                  <td className="px-6 py-4">
-                    <div className="flex items-center">
-                      <div>
-                        <p className="font-medium text-gray-900">Emirates</p>
-                        <p className="text-sm text-gray-500">Main</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-8 py-4 font-semibold text-gray-700">
-                    {tableData["Emirates"]
-                      ? tableData["Emirates"].capacity
-                      : "N/A"}
-                  </td>
-                  <td className="px-6 py-4">
-                    <span
-                      className={`inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold ${decideColor(
-                        tableData["Emirates"]
-                      )}`}
-                    >
-                      {tableData["Emirates"]
-                        ? tableData["Emirates"].load
-                        : "N/A"}
-                    </span>
-                  </td>
-                  <td className="px-6 py-4">
-                    <span className="inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold text-green-800 bg-green-300">
-                      {checkAvailability(tableData["Emirates"])
-                        ? "Online"
-                        : "Offline"}
-                    </span>
-                  </td>
-                </tr>
-                <tr className="hover:bg-gray-50">
-                  <td className="px-6 py-4">
-                    <div className="flex items-center">
-                      <div>
-                        <p className="font-medium text-gray-900">US</p>
-                        <p className="text-sm text-gray-500">Dallas</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-8 py-4 font-semibold text-gray-700">
-                    {tableData["US Dallas"]
-                      ? tableData["US Dallas"].capacity
-                      : "N/A"}
-                  </td>
-                  <td className="px-6 py-4">
-                    <span
-                      className={`inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold ${decideColor(
-                        tableData["US Dallas"]
-                      )}`}
-                    >
-                      {tableData["US Dallas"]
-                        ? tableData["US Dallas"].load
-                        : "N/A"}
-                    </span>
-                  </td>
-                  <td className="px-6 py-4">
-                    <span className="inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold text-green-800 bg-green-300">
-                      {checkAvailability(tableData["US Dallas"])
-                        ? "Online"
-                        : "Offline"}
-                    </span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </section>
+                  </div>
+                </td>
+                <td className="px-8 py-4 font-semibold text-gray-700">
+                  {tableData[location]?.capacity || "N/A"}
+                </td>
+                <td className="px-6 py-4">
+                  <span
+                    className={`inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold ${decideColor(
+                      tableData[location]
+                    )}`}
+                  >
+                    {tableData[location]?.load || "N/A"}
+                  </span>
+                </td>
+                <td className="px-6 py-4">
+                  <span
+                    className={`inline-flex items-center px-4 py-2 rounded-full text-xs font-semibold ${
+                      checkAvailability(tableData[location])
+                        ? "text-green-800 bg-green-300"
+                        : "text-red-800 bg-red-300"
+                    }`}
+                  >
+                    {checkAvailability(tableData[location]) ? "Online" : "Offline"}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
+  </section>
+</div>
   );
 }
